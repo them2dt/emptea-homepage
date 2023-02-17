739bc401073fa5b6bbe0144008ca30c2c10db2ba
them2dt <mtcreatives@hotmail.com>
2023-02-17 17:29:08 +0100

    add favicon and finishing touch on home-header

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,17 @@ import Footer from "../components/bricks/footer/Footer";
 
 //3rd-parties
 import { motion } from "framer-motion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <div className="home">
-      <Navbar />
-      <div className="home-header" id="home-header">
-        <div className="home-header-content">
-          <div className="home-header-logo">
+      <div className="home-header">
+        <div className="home-header-background">
+          <div className="home-header-background-left"></div>
+          <div className="home-header-background-right"></div>
+        </div>
+        <div className="home-header-foreground">
+          <div className="home-header-foreground-left">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{
@@ -27,30 +28,84 @@ export default function Home() {
                   stiffness: 500,
                 },
               }}
-              className="home-header-icon"
+              className="home-header-text"
+            >
+              <span>Emptea </span>builds
+              <br />
+              the next generation of
+              <span> products</span> &<span> services</span>, <br />
+              offering more
+              <br />
+              <span> freedom</span> and<span> independence</span>.
+            </motion.div>
+          </div>
+          <div className="home-header-foreground-right">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 0.5,
+                  duration: 1,
+                  damping: 25,
+                  stiffness: 500,
+                },
+              }}
+              className="home-header-graphic"
+            >
+              <Image src={web3_mockup} />
+            </motion.div>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="home-header">
+        <div className="home-header-background">
+          <div className="home-header-background-left"></div>
+          <div className="home-header-background-right"></div>
+        </div>
+        <div className="home-header-foreground">
+          <div className="home-header-foreground-left">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 0,
+                  duration: 1,
+                  damping: 25,
+                  stiffness: 500,
+                },
+              }}
+              className="home-header-text"
+            >
+              <span>Emptea </span>builds
+              <br />
+              the next generation of
+              <span> products</span> &<span> services</span>, <br />
+              offering more
+              <br />
+              <span> freedom</span> and<span> independence</span>.
+            </motion.div>
+          </div>
+          <div className="home-header-foreground-right">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: {
+                  delay: 0.5,
+                  duration: 1,
+                  damping: 25,
+                  stiffness: 500,
+                },
+              }}
+              className="home-header-graphic"
             >
               <Image src={web3_mockup} />
             </motion.div>
           </div>
         </div>
       </div>
-      <div className="about-us"></div>
-      <div className="suite">
-        <div className="suite-content">
-          <div className="suite-title">The Suite</div>
-          <div className="suite-grid">
-            <div className="suite-grid-cell">
-              <div className="suite-area-locked">
-                <div>
-                  <FontAwesomeIcon icon={faLock} />
-                  <div>Coming soon.</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
     </div>
   );
 }
