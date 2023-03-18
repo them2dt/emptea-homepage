d20e5b673b5e77d1fe3a73aa2869ecc0d1c01b94
them2dt <mtcreatives@hotmail.com>
2023-03-18 16:30:43 +0100

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -1,76 +1,31 @@
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import {
-  BackpackWalletAdapter,
-  CoinbaseWalletAdapter,
-  PhantomWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-import { clusterApiUrl } from "@solana/web3.js";
-import React, { useMemo } from "react";
+import React from "react";
 import Head from "next/head";
 
-import { SnackbarProvider } from "notistack";
-import { styled } from "@mui/material";
-
-import "../styles/navbar.css";
-import "../styles/footer.css";
 import "../styles/globals.css";
-import "../styles/adapter.css";
-import "../styles/section.css";
-
 import "../styles/home.css";
-import "../styles/suite.css";
 
 const App = ({ Component, pageProps }) => {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-  const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new BackpackWalletAdapter(),
-      new CoinbaseWalletAdapter(),
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [network]
-  );
-  const StyledSnackbarProvider = styled(SnackbarProvider)`
-    &.SnackbarItem-contentRoot {
-      background-color: var(--white);
-      color: var(--black);
-      font-family: "Inter Tight SemiBold";
-      font-size: 14px;
-      border-radius: 10px;
-    }
-
-    &.SnackbarItem-contentRoot button {
-      background-color: transparent;
-      border: 0px solid var(--white);
-      color: var(--black);
-      font-family: "Inter Tight Regular";
-      font-size: 14px;
-      cursor: pointer;
-    }
-    &.SnackbarItem-contentRoot button:hover {
-      color: var(--sign);
-      transition-duration: 0.2s;
-    }
-    &.SnackbarItem-contentRoot button:not(:hover) {
-      transition-duration: 0.2s;
-    }
-  `;
   return (
     <>
       <Head>
         <title>emptea</title>
-        <meta name="description" content="Move fast. Break free." />
+        <meta name="twitter:card" content="emptea" />
+        <meta name="twitter:site" content="emptea" />
+        <meta name="twitter:creator" content="@emptea" />
+        <meta name="twitter:title" content="emptea." />
+        <meta
+          name="twitter:description"
+          content="Building the next generation of products and services."
+        />
+        <meta
+          name="twitter:image:src"
+          content="https://bafkreicsctvtrwky6csbllm4dxox3634sj5wykbrx2ifhcdhymmuypcjae.ipfs.nftstorage.link/"
+        />
+        <meta name="title" content="emptea" />
+        <meta
+          name="description"
+          content="Building the next generation of products & services."
+        />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,14 @@
-//natives
-import Image from "next/image";
-import web3_mockup from "../components/bricks/media/web3-mockup.svg";
-import Footer from "../components/bricks/footer/Footer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 
-//3rd-parties
-import { motion } from "framer-motion";
-
-export default function Home() {
+export default function Index() {
   return (
     <div className="home">
-      <div className="home-header">
-        <div className="home-header-background">
-          <div className="home-header-background-left"></div>
-          <div className="home-header-background-right"></div>
-        </div>
-        <div className="home-header-foreground">
-          <div className="home-header-foreground-left">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 1,
-                transition: {
-                  delay: 0,
-                  duration: 1,
-                  damping: 25,
-                  stiffness: 500,
-                },
-              }}
-              className="home-header-text"
-            >
-              <span>Emptea </span>builds
-              <br />
-              the next generation of
-              <span> products</span> &<span> services</span>, <br />
-              offering more
-              <br />
-              <span> freedom</span> and<span> independence</span>.
-              <span>
-                <br />
-                <br />
-                <a href="#home-library">
-                  Learn more <FontAwesomeIcon icon={faArrowRight} />
-                </a>
-              </span>
-            </motion.div>
+      <div className="home-cover">
+        <div className="home-cover-content">
+          <div className="home-cover-description">
+            Building the next generation of products & services.
           </div>
-          <div className="home-header-foreground-right">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{
-                opacity: 1,
-                transition: {
-                  delay: 0.5,
-                  duration: 1,
-                  damping: 25,
-                  stiffness: 500,
-                },
-              }}
-              className="home-header-graphic"
-            >
-              <Image src={web3_mockup} />
-            </motion.div>
-          </div>
+          <div className="home-cover-title">This is emptea.</div>
         </div>
       </div>
     </div>
