29f7ad3215f24e6b68b8989401a11cf690b614af
maruthan4r <mtcreatives@hotmail.com>
2023-03-23 00:05:17 +0100

    add webs section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
+import Image from "next/image";
+
+import web_mockup from "../assets/Screen.gif";
+import web_1 from "../assets/Web-1.png";
+import web_2 from "../assets/Web-2.png";
+import web_3 from "../assets/Web-3.png";
 export default function Index() {
   useEffect(() => {
     console.log("Hello dev!");
@@ -11,21 +17,73 @@ export default function Index() {
 
   return (
     <div className="home">
-      <motion.div
-        className="home-cover"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.2 }}
-      >
-        <div className="home-cover-content">
-          <div className="home-cover-pre-text pre-text">
-            Building the next generation of products & services.
+      <div className="section home-intro">
+        <div className="section-content home-intro-content">
+          <div className="home-intro-left">
+            <div className="home-intro-left-content">
+              <div className="home-intro-pre-text pre-text">
+                Building the new generation of software.
+              </div>
+              <div className="home-intro-title title">
+                This is <span className="marked">emptea</span>.
+              </div>
+            </div>
           </div>
-          <div className="home-cover-title title">
-            This is <span className="marked"> emptea</span>.
+          <div className="home-intro-right">
+            <Image src={web_mockup} />
           </div>
         </div>
-      </motion.div>
+      </div>
+      <div className="section home-webs">
+        <div className="section-content home-webs-content">
+          <div className="home-webs-headline headline-1">
+            We build trust<span className="marked">less</span>.
+          </div>
+          <div className="home-webs-grid">
+            <div className="home-web">
+              <div className="home-web-content">
+                <div className="home-web-image">
+                  <Image src={web_1} />
+                </div>
+                <div className="home-web-headline headline-2">Web 1.0</div>
+                <div className="home-web-text text">
+                  The original. One username, one password. easily hackable.
+                </div>
+              </div>
+            </div>
+            <div className="home-web">
+              <div className="home-web-content">
+                <div className="home-web-image">
+                  <Image src={web_2} />
+                </div>
+                <div className="home-web-headline headline-2">Web 2.0</div>
+                <div className="home-web-text text">
+                  Platforms integrate social logins. You have now thousands of
+                  options to log in to a platform. Choose wisely.
+                </div>
+              </div>
+            </div>
+            <div className="home-web">
+              <div className="home-web-content">
+                <div className="home-web-image">
+                  <Image src={web_3} />
+                </div>
+                <div className="home-web-headline headline-2 marked">
+                  Web 3.0
+                </div>
+                <div className="home-web-text text">
+                  One log-in for everything. Instead of vulnerable accounts, you
+                  now have a encrypted wallet. You don't log in, you sign
+                  transactions.
+                  <br />
+                  <br />
+                  No database to attack, no passwords to steal.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="social-widget">
         <div className="social-widget-content">
           <Link href={"https://twitter.com/empteaxyz"}>
