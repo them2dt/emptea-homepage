1357d1982e6b00791c8937c956cc8b3dbf5b8593
maruthan4r <mtcreatives@hotmail.com>
2023-03-23 14:42:03 +0100

    add product-section

--- pages/index.js
+++ pages/index.js
@@ -16,84 +16,173 @@ export default function Index() {
   }, []);
 
   return (
-    <div className="home">
-      <div className="section home-intro">
-        <div className="section-content home-intro-content">
-          <div className="home-intro-left">
-            <div className="home-intro-left-content">
-              <div className="home-intro-pre-text pre-text">
+    <motion.div className="home">
+      <motion.div className="section home-intro">
+        <motion.div className="section-content home-intro-content">
+          <motion.div className="home-intro-left">
+            <motion.div className="home-intro-left-content">
+              <motion.div
+                className="home-intro-pre-text pre-text"
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+              >
                 Building the new generation of software.
-              </div>
-              <div className="home-intro-title title">
+              </motion.div>
+              <motion.div
+                className="home-intro-title title"
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+              >
                 This is <span className="marked">emptea</span>.
-              </div>
-            </div>
-          </div>
-          <div className="home-intro-right">
-            <Image src={web_mockup} alt="web mockup"/>
-          </div>
-        </div>
-      </div>
-      <div className="section home-webs">
-        <div className="section-content home-webs-content">
-          <div className="home-webs-headline headline-1">
-            Don{"&apos;"}t trust, <span className="marked">verify</span>.
-          </div>
-          <div className="home-webs-grid">
-            <div className="home-web">
-              <div className="home-web-content">
-                <div className="home-web-image">
-                  <Image src={web_1}  alt="web 1 login"/>
-                </div>
-                <div className="home-web-headline headline-2">Web 1.0</div>
-                <div className="home-web-text text">
+              </motion.div>
+            </motion.div>
+          </motion.div>
+          <motion.div
+            className="home-intro-right"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+          >
+            <Image src={web_mockup} alt="web mockup" />
+          </motion.div>
+        </motion.div>
+      </motion.div>
+      <motion.div className="section home-webs">
+        <motion.div className="section-content home-webs-content">
+          <motion.div
+            className="home-webs-headline headline-1"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+          >
+            Don't trust, <span className="marked">verify</span>.
+          </motion.div>
+          <motion.div className="home-webs-grid">
+            <motion.div
+              className="home-web"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0 }}
+            >
+              <motion.div className="home-web-content">
+                <motion.div className="home-web-image">
+                  <Image src={web_1} alt="web 1 login" />
+                </motion.div>
+                <motion.div className="home-web-headline headline-2">
+                  Web 1.0
+                </motion.div>
+                <motion.div className="home-web-text text">
                   The original. One username, one password. easily hackable.
-                </div>
-              </div>
-            </div>
-            <div className="home-web">
-              <div className="home-web-content">
-                <div className="home-web-image">
-                  <Image src={web_2} alt="web 2 login"/>
-                </div>
-                <div className="home-web-headline headline-2">Web 2.0</div>
-                <div className="home-web-text text">
+                </motion.div>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              className="home-web"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.4 }}
+            >
+              <motion.div className="home-web-content">
+                <motion.div className="home-web-image">
+                  <Image src={web_2} alt="web 2 login" />
+                </motion.div>
+                <motion.div className="home-web-headline headline-2">
+                  Web 2.0
+                </motion.div>
+                <motion.div className="home-web-text text">
                   Platforms integrate social logins. You have now thousands of
                   options to log in to a platform. Choose wisely.
-                </div>
-              </div>
-            </div>
-            <div className="home-web">
-              <div className="home-web-content">
-                <div className="home-web-image">
-                  <Image src={web_3} alt="web 3 login"/>
-                </div>
-                <div className="home-web-headline headline-2 marked">
+                </motion.div>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              className="home-web"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.8 }}
+            >
+              <motion.div className="home-web-content">
+                <motion.div className="home-web-image">
+                  <Image src={web_3} alt="web 3 login" />
+                </motion.div>
+                <motion.div className="home-web-headline headline-2 marked">
                   Web 3.0
-                </div>
-                <div className="home-web-text text">
+                </motion.div>
+                <motion.div className="home-web-text text">
                   One log-in for everything. Instead of 100 vulnerable accounts,
-                  you now have one encrypted wallet. You don{'&apos;'}t log in, you
+                  you now have one encrypted wallet. You don't log in, you
                   approve transactions.
                   <br />
                   <br />
                   No database to attack, no passwords to steal.
+                </motion.div>
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+      <div className="section home-products">
+        <div className="section-content">
+          <div className="home-products-headline headline-1">
+            Explore our products
+          </div>
+          <div className="home-products-grid-container">
+            <div className="home-products-grid">
+              <div className="home-product">
+                <div className="home-product-info">
+                  <div className="home-product-headline headline-3">
+                    Emptea Campaigns
+                  </div>
+                  <div className="home-product-description text">
+                    Crowdfunding-platform on solana.
+                  </div>
+                </div>
+                <div className="home-product-link text">
+                  <Link href={"https://campaigns.emptea.xyz"}>Explore</Link>
+                </div>
+              </div>
+              <div className="home-product">
+                <div className="home-product-info">
+                  <div className="home-product-headline headline-3">
+                    Emptea Oxygen
+                  </div>
+                  <div className="home-product-description text">
+                    Mint NFT's on solana.
+                  </div>
+                </div>
+                <div className="home-product-link text">
+                  <Link href={"https://oxygen.emptea.xyz"}>Explore</Link>
+                </div>
+              </div>
+              <div className="home-product">
+                <div className="home-product-info">
+                  <div className="home-product-headline headline-3">
+                    Emptea Library
+                  </div>
+                  <div className="home-product-description text">
+                    Read, write and collect written content on solana.
+                  </div>
+                </div>
+                <div className="home-product-link text">
+                  <Link href={"https://library.emptea.xyz"}>Explore</Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="social-widget">
-        <div className="social-widget-content">
+      <motion.div className="social-widget">
+        <motion.div className="social-widget-content">
           <Link href={"https://twitter.com/empteaxyz"}>
             <FontAwesomeIcon icon={faTwitter} />
           </Link>
           <Link href={"mailto:info@emptea.xyz"}>
             <FontAwesomeIcon icon={faEnvelope} />
           </Link>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
