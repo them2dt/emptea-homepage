6c96a1666f4aac7443465c8fd1c9a429136667fb
maruthan4r <mtcreatives@hotmail.com>
2023-09-30 21:37:52 +0200

    add page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import "../styles/navbar.css";
 import "../styles/globals.css";
 import "../styles/home.css";
+import "../styles/atelier.css";
 
 import Head from "next/head";
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+
 export default function Index() {
   return (
     <div className="app">
-      <div className="intro">
-        <div className="title">emptea studios.</div>
-        <div className="navbar">
-          <a href=""></a>
+      <div className="home" id="home">
+        <motion.div
+          className="title"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 0.2,
+            delay: 0.4,
+          }}
+        >
+          emptea studios
+        </motion.div>
+        <motion.div
+          className="slogan"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 0.2,
+            delay: 0.8,
+          }}
+        >
+          Elevating <span className="slogan-highlight">webscapes.</span>
+        </motion.div>
+        <div className="navbar-container">
+          <motion.div
+            className="navbar"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              duration: 0.2,
+              delay: 0.6,
+            }}
+          >
+            <Link href={"/atelier"} onClick={()=>{}}>ateliér</Link>
+            <Link href={"/atelier"}>services</Link>
+            <Link href={"/atelier"}>contact</Link>
+          </motion.div>
         </div>
       </div>
     </div>
