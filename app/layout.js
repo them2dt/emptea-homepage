37dc41c76d1dd89d4b3eb3cea84a13120360686e
maruthan4r <mtcreatives@hotmail.com>
2024-01-19 10:00:37 +0100

    add head

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import "./styles/team.css";
 import "./styles/collabs.css";
 import "./styles/footer.css";
 
+import Head from "next/head";
+
 export const metadata = {
   title: "Emptea Studios",
   description: "Conquer horizons.",
@@ -13,6 +15,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Emptea Studios</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@EmpteaStudios" />
+        <meta name="twitter:title" content="Emptea Studios" />
+        <meta name="twitter:description" content="Conquer horizons." />
+        <meta
+          name="twitter:image:alt"
+          content="A cover image for the website."
+        />
+        <meta
+          name="twitter:image"
+          content="https://bafkreifzxtg23dbae6xw5vsazrwhgbf2h25kymlct4lsgqauxakvs46kfa.ipfs.nftstorage.link/"
+        />
+
+        <meta property="og:title" content="Emptea Studios" />
+        <meta property="og:description" content="Conquer horizons." />
+        <meta
+          property="og:image"
+          content="https://bafkreifzxtg23dbae6xw5vsazrwhgbf2h25kymlct4lsgqauxakvs46kfa.ipfs.nftstorage.link/"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1000" />
+        <meta property="og:image:height" content="500" />
+      </Head>
       <body>{children}</body>
     </html>
   );
