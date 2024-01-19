d2f9137464181901f6fbd8361c6299a3a692ab4f
maruthan4r <mtcreatives@hotmail.com>
2024-01-19 10:30:00 +0100

    i hate nextjs

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,26 @@ import Head from "next/head";
 export const metadata = {
   title: "Emptea Studios",
   description: "Conquer horizons.",
+  openGraph: {
+    title: "Emptea Studios",
+    description: "Conquer horizons.",
+    url: "https://emptea.xyz",
+    siteName: "Emptea Studios",
+    images: [
+      {
+        url: "https://bafkreifzxtg23dbae6xw5vsazrwhgbf2h25kymlct4lsgqauxakvs46kfa.ipfs.nftstorage.link/",
+        width: "1000",
+        height: "500",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Emptea Studios.",
+    description: "Conquer horizons.",
+    creator: "@EmpteaStudios",
+    images: ["https://bafkreifzxtg23dbae6xw5vsazrwhgbf2h25kymlct4lsgqauxakvs46kfa.ipfs.nftstorage.link/"],
+  },
 };
 
 export default function RootLayout({ children }) {
