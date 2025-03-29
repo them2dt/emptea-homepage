8c1e89332fcaf55f1c307fca9e0b1234569f8d21
Maru <mtcreatives@hotmail.com>
2025-03-29 12:19:16 +0100

    minimalize

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import "./styles/page.css";
+export default function Home() {
+  return (
+    <main className="main">
+      
+      {/* Section 1 */}
+      <section className="section section-1">
+        <div>section-1</div>
+      </section>
+
+      {/* Section 2 */}
+      <section className="section section-2">
+        <div>section-2</div>
+      </section>
+
+      {/* Section 3 */}
+      <section className="section section-3">
+        <div>section-3</div>
+      </section>
+
+      {/* Section 4 */}
+      <section className="section section-4">
+        <div>section-4</div>
+      </section>
+
+      {/* Section 5 */}
+      <section className="section section-5">
+      </section>
+    </main>
+  );
+}
