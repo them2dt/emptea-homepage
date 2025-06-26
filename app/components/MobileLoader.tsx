6cf1ec0a6d541e8c32bf07eaf5f037acfabcd17b
Maru <mtcreatives@hotmail.com>
2025-06-26 18:50:24 +0200

    improve loader (responsibility)

--- app/components/MobileLoader.tsx
+++ app/components/MobileLoader.tsx
@@ -9,7 +9,7 @@ const MobileLoader = () => {
       exit={{ opacity: 0, transition: { duration: 0.5, ease: 'easeOut' } }}
     >
       <motion.div
-        className={styles.loaderText}
+        className={styles.mobileLoaderText}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, ease: 'easeInOut' }}
