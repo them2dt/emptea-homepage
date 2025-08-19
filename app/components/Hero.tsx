c1134aa21b3a48f9ec7214fb857d1c686d6f4077
Maru <mtcreatives@hotmail.com>
2025-08-19 20:50:16 +0200

    update Hero component height and adjust global styles for consistent black background and white text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen text-center pt-30 bg-black bg-[linear-gradient(rgba(255,80,0,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(255,80,0,0.1)_1px,transparent_1px)] bg-[size:40px_40px] relative overflow-hidden border-b border-white/10">
+    <section className="flex flex-col items-center justify-center h-[75vh] text-center pt-30 bg-black bg-[linear-gradient(rgba(255,80,0,0.1)_1px,transparent_1px),linear-gradient(90deg,rgba(255,80,0,0.1)_1px,transparent_1px)] bg-[size:40px_40px] relative overflow-hidden border-b border-white/10">
       <div className="font-bold text-7xl mb-8 text-white leading-tight relative z-10">
         Modern software <br /> <span className="text-orange-500 relative">built to scale.</span>
       </div>
